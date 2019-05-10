7e3509ccaa3bfa9eca35d2422e1deaeaf431aa3d
pawan421 <pawan@appster.in>
2019-05-10 09:34:18 +0530

    DSAlgoriths added

---
+++ DSAlgorithms.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+
+class DSAlgorithms {
+
+    
+    //default
+    public static let `default` = DSAlgorithms()
+
+    
+    func testing () {
+        print("testing ")
+    }
+    
+    private init () {
+        
+    }
+ 
+}
+
+
+
+DSAlgorithms.default.testing()
