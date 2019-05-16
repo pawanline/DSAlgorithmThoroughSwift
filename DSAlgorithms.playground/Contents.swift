10b122e3a38ac09e8e6c052aea554d823d155c22
pawan421 <pawan@appster.in>
2019-05-16 08:50:00 +0530

    Count the characters

--- DSAlgorithms.playground/Contents.swift
+++ DSAlgorithms.playground/Contents.swift
@@ -4,13 +4,53 @@ import UIKit
 class DSAlgorithms {
 
     
+    private let created = Date()
+    
+    
+    
+    func executionDuration() {
+        let diff = Date().timeIntervalSince(created)
+        print("alogrithm takes \(diff) seconds")
+    }
+    
+    
+    func executionStart() {
+        print("created at: \(created)")
+    }
+    
+   
+    
     //default
     public static let `default` = DSAlgorithms()
 
     
-    func testing () {
-        print("testing ")
+    
+    //1:  Count the characters
+  
+    // brute force methods
+    func countTheCharacter(_ str: String,letter: Character) -> Int {
+        var letterCount: Int = 0
+        for chr in str {
+            if chr == letter {
+                letterCount += 1
+            }
+        }
+        
+        return letterCount
     }
+    
+    
+    // Using reduce methods
+    
+    func countTheCharacterUsingReduce(_ str: String,letter: Character) -> Int {
+        return str.reduce(0){
+            $1 == letter ? $0 + 1 : $0
+        }
+        
+    }
+
+    
+    
     
     private init () {
         
@@ -20,4 +60,15 @@ class DSAlgorithms {
 
 
 
-DSAlgorithms.default.testing()
+DSAlgorithms.default.executionStart()
+
+// Count the characters using reduce methods
+DSAlgorithms.default.countTheCharacter("India is great country", letter: "a")
+
+DSAlgorithms.default.executionDuration()
+
+DSAlgorithms.default.executionStart()
+
+DSAlgorithms.default.countTheCharacterUsingReduce("India is great country", letter: "a")
+
+DSAlgorithms.default.executionDuration()
