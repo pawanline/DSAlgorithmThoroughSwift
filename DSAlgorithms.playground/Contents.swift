e6eb9e0b8cf130cb11ca977fba82d2ad259c01d1
pawan421 <pawan@appster.in>
2019-08-07 09:26:57 +0530

    closure logic

--- DSAlgorithms.playground/Contents.swift
+++ DSAlgorithms.playground/Contents.swift
@@ -119,3 +119,75 @@ print(DSAlgorithms.default.leftShifting(arr: &arr, movement: 3))
 print(" left shifting \(arr)")
 
 print("dictionary value : \(DSAlgorithms.default.getOccurences(arr: [1,1,2,3,4,5,12,3,4,3,1,2,3]))")
+
+
+
+var str = "Hello, playground"
+
+
+func calculateSum(arr:[Int],result: ((Int) -> Void)?) {
+    result?(arr.reduce(0) { $0 + $1 } )
+}
+
+
+
+calculateSum(arr: [1,2,3,4]) { (result) in
+    print("sum of numbers is \(result)")
+}
+
+
+
+let driving = { (place:String) in
+    print("driving in \(place)")
+}
+
+
+driving("london")
+
+
+func whoIsDriving(name: String,driving: ((String) -> (Void))? ) {
+    
+    driving?("\(name) is driving")
+}
+
+
+whoIsDriving(name: "Pawan") { (str) -> (Void) in
+    print(str, separator: " ", terminator: "")
+}
+
+
+let calculateMaturity = { (age: Int) -> String in
+    
+    if age >= 18 {
+        return "Mature"
+    } else {
+        return "Minor"
+    }
+    
+}
+
+
+
+let running = {
+    print("I am running")
+}
+
+
+func activity(action: () -> Void) {
+    print("\n I am freshen up")
+    print("I got ready")
+    action()
+}
+
+print(activity(action: running))
+
+
+let driveSafely = {  () -> String in
+    return "I'm being a considerate driver"
+}
+func drive(using driving: () -> String) {
+    print("Let's get in the car")
+    driving()
+    print("We're there!")
+}
+drive(using: driveSafely)
